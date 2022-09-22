f6e2d25eefc8995570489d318ea206bb263c7a3e
samuel powers <powerss7@mail.uc.edu>
2022-09-22 15:46:05 -0400

    it works

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double NumOne = 11;
+        double NumTwo = 13;
+
+        if (NumOne == NumTwo)
+        {
+            System.out.println("you numbers are the same");
+        }
+        else if (NumOne > NumTwo)
+        {
+            System.out.println(NumOne + "is greater");
+        }
+        else if (NumOne < NumTwo)
+        {
+            System.out.println(NumTwo + "is greater");
+        }
     }
 }
